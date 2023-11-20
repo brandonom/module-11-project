06ca4684726a83d9081d771dc6171c3aad0926f2
brandonom <brandonomyers1@gmail.com>
2023-11-20 03:22:18 -0500

    added get notes function

--- server.js
+++ server.js
@@ -9,3 +9,14 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 const dbPath = path.join(__dirname, "db.json");
+
+app.get("/api/notes", function (req, res) {
+    getData()
+      .then((notes) => {
+        res.json(notes);
+      })
+      .catch((err) => {
+        console.error("Error getting data:", err);
+        res.status(500).send("Internal Server Error");
+      });
+  });
